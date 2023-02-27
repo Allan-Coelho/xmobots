01853c00e9909f234fd875992ea98b294a46f911
Allan Vieira Coelho <github@allanvieira.dev>
2023-02-27 11:29:13 -0300

    refactor: make another component to host map

--- src/features/MapPage/MapPage.js
+++ src/features/MapPage/MapPage.js
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { MapWrapper, Sidebar, User, Username, Wrapper } from "./styles";
+import { Sidebar, User, Username, Wrapper } from "./styles";
+import { Map } from "./Map";
 import { Avatar, Modal } from "antd";
 import { FileUpload } from "../FileUpload/FileUpload";
 import { AerodromesTable } from "../FileUpload/AerodromesTable";
@@ -43,23 +43,7 @@ export function MapPage() {
           <AerodromesTable content={content} />
         </Modal>
       </Sidebar>
-      <MapWrapper>
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </MapWrapper>
+      <Map content={content} />
     </Wrapper>
   );
 }
